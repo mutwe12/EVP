b24cb3db4fad76e398ea4a528d65ae8d3180ccbc
Your Name <ishimwefab250@gmail.com>
2024-04-28 06:05:33 -0700

    edit add

--- src/pages/about/OurStory.jsx
+++ src/pages/about/OurStory.jsx
@@ -15,7 +15,12 @@ const OurStory = () => {
         Professional service to Bangladesh  LOVE TO PLAN
       </p> */}
       <p className="text-xl ">
-      We are an exclusive, independent event planning company based in Bangladesh, specializing in premium wedding services, birthday parties, and perfect party organizing tailored to the needs of our clients. With a touch of personalization, we create stylish, exclusive, and creative events throughout Bangladesh. Our award winning team of event management and wedding planners offers a highly creative, friendly, and professional service dedicated to making every event memorable. At Bangladesh Love to Plan, we are passionate about bringing dreams to life.
+      We are an exclusive, independent event planning company based in Kigali, specializing in premium wedding services, birthday parties, and perfect party organizing tailored to the needs of our clients. 
+      With a touch of personalization, we create stylish, exclusive,
+       and creative events throughout Bangladesh. Our award winning 
+       team of event management and wedding planners offers a highly creative, 
+       friendly, and professional service dedicated to making every event memorable.
+        At Rwandan Love to Plan, we are passionate about bringing dreams to life.
       </p>
     </div>
     </Container>
