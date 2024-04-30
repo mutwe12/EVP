d6591855c25b555e0c76a74566ce27ffe1136b46
Your Name <ishimwefab250@gmail.com>
2024-04-30 09:59:28 -0700

    i change the log

--- src/components/cards/EventCards.jsx
+++ src/components/cards/EventCards.jsx
@@ -23,8 +23,8 @@ const EventCards = () => {
               onClick={goToAllServices}
               className="text-xl border px-6 py-2 rounded-3xl hover:bg-gradient-to-r from-purple-500 to-emerald-300 hover:text-white border-cyan-500"
             >
-              View All
-            </button>
+              View All Event
+            </button> 
           </div>
         </div>
       </div>
